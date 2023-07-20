6055bbbe9fa3ef408b35418b1a95829d8d54c179
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-07-20 12:35:05 -0300

    fix domain cors front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import poligonoRoutes from "./routes/poligono.routes";
 import coordenadaRoutes from "./routes/coordenada.routes";
 var corsOptions = {
-  origin: "react-leaflet-yz3w-git-main-r0l0ct.vercel.app",
+  origin: "https://react-leaflet-yz3w-git-main-r0l0ct.vercel.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors(corsOptions));
 app.use(function (_req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "react-leaflet-yz3w-git-main-r0l0ct.vercel.app"
+    "https://react-leaflet-yz3w-git-main-r0l0ct.vercel.app"
   );
   res.header(
     "Access-Control-Allow-Headers",

05cf5c1ee5224b91b2637b40a6d1a9000911df76
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-07-20 15:16:56 -0300

    fix prisma client

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: "postgres://rolo:Qxee5lan4dfVdvscSwdi7v6D0hyZKrrb@dpg-circdu5iuie930maplfg-a.oregon-postgres.render.com/mapadb",
+    },
+  },
+});
 
 export default prisma;

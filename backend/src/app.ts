a492e3fc0fa81537a41892d5c0aa7b0f84a5366c
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-07-20 13:06:05 -0300

    add some cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
 import poligonoRoutes from "./routes/poligono.routes";
@@ -29,18 +29,24 @@ app.use(
     credentials: true,
   })
 );
-// app.use(function(req, res, next) {
-//       // res.header("Access-Control-Allow-Origin", "*");
-//       const allowedOrigins = ['http://localhost:3000',"https://react-leaflet-yz3w-git-main-r0l0ct.vercel.app" ];
-//       const origin = req.headers.origin;
-//       if (allowedOrigins.includes(origin)) {
-//            res.setHeader('Access-Control-Allow-Origin', origin);
-//       }
-//       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//       res.header("Access-Control-Allow-credentials", true);
-//       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-//       next();
-//     });
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://react-leaflet-yz3w-git-main-r0l0ct.vercel.app",
+  ];
+  const origin = req.headers.origin as string;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 app.use(express.json());
 
 app.use("/", poligonoRoutes);

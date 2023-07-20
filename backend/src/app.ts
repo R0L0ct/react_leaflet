c6a2e4a60642d4a0ffabfc6ce4aa414c18c5f944
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-07-20 12:30:27 -0300

    fix corsOptions back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,20 +3,23 @@ import cors from "cors";
 
 import poligonoRoutes from "./routes/poligono.routes";
 import coordenadaRoutes from "./routes/coordenada.routes";
-// var corsOptions = {
-//   origin: "*",
-//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
+var corsOptions = {
+  origin: "react-leaflet-yz3w-git-main-r0l0ct.vercel.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 const app = express();
-app.use(cors());
-// app.use(function (_req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(cors(corsOptions));
+app.use(function (_req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "react-leaflet-yz3w-git-main-r0l0ct.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 app.use("/", poligonoRoutes);

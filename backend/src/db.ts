63d79f57a3b317a3e65bcfafec0ffd02b4988838
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-07-20 15:30:48 -0300

    remove "" from data url

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: "postgres://rolo:Qxee5lan4dfVdvscSwdi7v6D0hyZKrrb@dpg-circdu5iuie930maplfg-a.oregon-postgres.render.com/mapadb",
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
